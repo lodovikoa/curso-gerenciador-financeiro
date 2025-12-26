14f68915ad493356898b53567cea77f2d0ff49a5
lodoviko <lodoviko@gmail.com>
2025-12-26 19:03:17 -0300

    feat: 20261226 - aulas do curso

---
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -0,0 +1,31 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+type CardType = 'income' | 'outcome' | 'balance';
+type ValueCssClass = 'income' | 'outcome';
+
+@Component({
+  selector: 'app-balance-card',
+  imports: [MatCardModule],
+  templateUrl: './balance-card.html',
+  styleUrl: './balance-card.scss',
+})
+export class BalanceCard {
+  type  = input.required<CardType>();
+  label = input.required<string>();
+  value = input.required<number>();
+
+
+  cssClass = computed<ValueCssClass>(() => {
+    if(this.type() === 'income') {
+      return 'income';
+    }
+
+    if(this.type() === 'outcome') {
+      return 'outcome';
+    }
+
+    return this.value() > 0 ? 'income': 'outcome';
+  });
+
+}
