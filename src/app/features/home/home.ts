cae3a3ca0efaa0028890a01f5a28b9d6f22330da
Lodovico Ardissao <lodoviko@gmail.com>
2025-12-30 18:05:46 -0300

    feat: 20251230 - Adicionar margem

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,20 +3,21 @@ import { Balance } from "./components/balance/balance";
 import { TransactionItem } from "./components/transaction-item/transaction-item";
 import { Transaction } from '../../shared/transaction/interfaces/transactions';
 import { TransactionType } from '../../shared/transaction/enum/transaction-type';
+import { NoTransactions } from "./components/no-transactions/no-transactions";
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
   transactions = signal<Transaction[]>([
-    { title: 'Salario', value: 100, type: TransactionType.INCOME },
-    { title: 'VA', value: 50, type: TransactionType.OUTCOME },
-    { title: 'Aluguel', value: 100, type: TransactionType.OUTCOME },
-    { title: 'Extra', value: 50, type: TransactionType.INCOME },
+    // { title: 'Salario', value: 100, type: TransactionType.INCOME },
+    // { title: 'VA', value: 50, type: TransactionType.OUTCOME },
+    // { title: 'Aluguel', value: 100, type: TransactionType.OUTCOME },
+    // { title: 'Extra', value: 50, type: TransactionType.INCOME },
   ]);
 
 }
