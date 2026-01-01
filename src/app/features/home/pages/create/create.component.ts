e296d77353dc63cf9c5c1c53139269893871bcbe
Lodovico Ardissao <lodoviko@gmail.com>
2026-01-01 08:38:07 -0300

    feat:20260101 - Criando o formulario

---
+++ src/app/features/home/pages/create/create.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from "@angular/material/button";
+
+@Component({
+  selector: 'app-create',
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss',
+})
+export class CreateComponent {
+
+  form = new FormGroup({
+    title: new FormControl('', { validators: [Validators.required] }),
+    value: new FormControl('', { validators: [Validators.required] })
+   });
+
+}
--- src/app/features/home/routes.ts
+++ src/app/features/home/routes.ts
@@ -1,6 +1,8 @@
+import { CreateComponent } from './pages/create/create.component';
 import { Routes } from "@angular/router";
 import { Home } from "./home";
 
 export default [
-    { path: '', component: Home }
+    { path: '', component: Home },
+    { path: 'create', component: CreateComponent }
 ] as Routes
