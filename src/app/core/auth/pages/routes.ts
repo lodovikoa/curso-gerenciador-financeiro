8780d5fca0fd9d175d421115d1c7cf85cc4847b8
Lodovico Ardissao <lodoviko@gmail.com>
2026-01-07 09:19:02 -0300

    feat: 20260107 - layout da tela de login

--- src/app/core/auth/pages/routes.ts
+++ src/app/core/auth/pages/routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
+import { LayoutComponent } from "../components/layout/layout.component";
 
 export default [
-  { path: 'login', component: LoginComponent }
+  { path: '', component: LayoutComponent, children: [
+    { path: 'login', component: LoginComponent },
+  ], },
 ] as Routes;
