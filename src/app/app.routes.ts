d9bbbebc52f1574b6d240440b0f0f9edfa9b2c7a
Lodovico Ardissao <lodoviko@gmail.com>
2026-01-09 08:32:27 -0300

    feat: 20260109 - Criando guard para proteger a aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { isAuthenticatedGuard } from './core/auth/guards/is-authenticated-guard';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./core/layout/layout').then(m => m.Layout),
+    { path: '', canActivate: [ isAuthenticatedGuard ], loadComponent: () => import('./core/layout/layout').then(m => m.Layout),
       children:[{ path: '', loadChildren: () => import('./features/home/routes') }],
      },
     { path: 'auth', loadChildren: () => import('./core/auth/pages/routes') }
