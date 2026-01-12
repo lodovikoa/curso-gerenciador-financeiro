e42e788271d4050dc0d08c9d19d327a0a692be2e
Lodovico Ardissao <lodoviko@gmail.com>
2026-01-12 09:15:03 -0300

    feat: 20260112 - config token

---
+++ src/app/core/auth/interceptors/set-auth-token-interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { LoggedInUserStoreService } from '../stores/logged-in-user-store.service';
+import { inject } from '@angular/core';
+import { AuthTokenStorageService } from '../services/auth-token-storage.service';
+
+export const setAuthTokenInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const loggedInUserStoreService = inject(LoggedInUserStoreService);
+
+  if(!loggedInUserStoreService.isLoggedIn()) {
+    return next(req);
+  }
+
+  const authTokenStorageService = inject(AuthTokenStorageService);
+
+  const token = authTokenStorageService.get() as string;
+
+  const newReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return next(newReq);
+};
