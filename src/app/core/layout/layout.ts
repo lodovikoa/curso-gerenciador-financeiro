2d6c7f5782fb5f089690bc39356aa811f25bd215
Lodovico Ardissao <lodoviko@gmail.com>
2026-01-23 09:54:46 -0300

    feat: 20260123 - Sidenav - iniciando configuracao de menu

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { Header } from "./components/header/header";
 import { RouterOutlet } from "@angular/router";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, MatSidenavModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
