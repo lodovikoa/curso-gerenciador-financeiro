53c75dcccc193a3e1768d29ca9b256f3172de371
Lodovico Ardissao <lodoviko@gmail.com>
2026-01-24 14:33:55 -0300

    feat: 20260124 refatorando menu

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,18 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
-import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterOutlet } from "@angular/router";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
+import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet, MatSidenavModule, MatListModule, RouterLink, RouterLinkActive],
+  imports: [Header, RouterOutlet, MatSidenavModule, MatListModule, SidenavItemsComponent],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
 export class Layout {
-
-  links = signal([
-    { label: 'Home', url: '/' },
-  ]);
 }
